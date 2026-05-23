8c44be5873c697011aac596400171fdd1b9df410
Musira-Hafsa <musirahamna@gmail.com>
2026-05-23 11:48:06 +0500

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
- withCredentials: true,
+baseURL: 'https://blogsync-backend.vercel.app'
+ 
 });
 
 // Attach Bearer token from storage if present
