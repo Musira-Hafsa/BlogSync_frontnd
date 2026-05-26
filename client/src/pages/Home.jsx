026c691b50d8b46088979241d3fdfd41dc157255
Musira-Hafsa <musirahamna@gmail.com>
2026-05-26 07:05:34 +0500

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -390,13 +390,13 @@ export function Writers() {
     })();
   }, []);
 
-  // const handleLogout = () => {
-  //   ["bs_token", "bs_user"].forEach((k) => {
-  //     localStorage.removeItem(k);
-  //     sessionStorage.removeItem(k);
-  //   });
-  //   navigate("/auth");
-  // };
+  const handleLogout = () => {
+    ["bs_token", "bs_user"].forEach((k) => {
+      localStorage.removeItem(k);
+      sessionStorage.removeItem(k);
+    });
+    navigate("/auth");
+  };
 
   const handleFollowAction = async (author, currentlyFollowing) => {
     if (!storedUser) {
@@ -518,10 +518,10 @@ export default function Home() {
     feedRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
   };
 
-    const [user, setUser] = useState(() => {
-    const savedUser = localStorage.getItem("bs_user") || sessionStorage.getItem("bs_user");
-    return savedUser ? JSON.parse(savedUser) : null;
-  });
+  //   const [user, setUser] = useState(() => {
+  //   const savedUser = localStorage.getItem("bs_user") || sessionStorage.getItem("bs_user");
+  //   return savedUser ? JSON.parse(savedUser) : null;
+  // });
 
   // ==========================================
   // FETCH BLOGS LOGIC (YOURS UPDATED WITH STATE CHECK)
